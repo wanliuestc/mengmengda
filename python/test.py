1d4801cc444536d584e22fb792b8873f1f45c22d
root <root@localhost.localdomain>
2017-10-24 17:06:46 -0500

    2017/10/24 22:08

---
+++ python/test.py
@@ -0,0 +1,11 @@
+#!/usr/local/python3
+classmate = ['one', 'two', 'three'];
+print(classmate[0]);
+classmate.append('four')
+print(len(classmate))
+classmate.insert(0, 'ttt')
+print(classmate)
+classmate.insert(8, '666')
+print(classmate)
+classmate.pop()
+print(classmate)
