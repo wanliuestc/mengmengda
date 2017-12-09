da650812cc3ae6fe0de380f69d796e5637466157
wanliuestc <changwanli777@163.com>
2017-12-09 03:02:05 -0500

    20171209

--- python/oop1.py
+++ python/oop1.py
@@ -1,13 +1,17 @@
 class Student(object):
     def __init__(self, name, score):
-        self.name = name
-        self.score = score
+        self.__name = name
+        self.__score = score
 
     def print_score(self):
-        print('%s:%s' % (self.name, self.score))
+        print('%s:%s' % (self.__name, self.__score))
 
 Tom = Student('Tom', 56)
 Jack = Student('Jack', 99)
 
+
 Tom.print_score()
 Jack.print_score()
+
+print(dir(Tom))
+print(Tom._Student__name)
